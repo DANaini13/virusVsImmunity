02034b2d7f2219669c487b87794a8cab3615cfd1
Zeyong Shan <shan27@pdx.edu>
2020-06-11 00:09:26 +0800

    拆分角色定义

--- Assets/Scripts/Battle/BattleModel.cs
+++ Assets/Scripts/Battle/BattleModel.cs
@@ -31,25 +31,6 @@ class PlayerOperation
     public List<PlayerArg> argList;
 }
 
-abstract class Player 
-{
-    protected vector2 location;
-    protected decimal speed;
-    public abstract void move(vector2 direction);
-    public abstract void implementTask(int key);
-} 
-class Macrophages: Player
-{
-    public override void implementTask(int key)
-    {
-    }
-
-    public override void move(vector2 direction)
-    {
-        location = vector2.plus(location, vector2.mutiply(direction, speed));
-    }
-}
-
 class ModelMessageWrapper
 {
     public ModelMessageWrapper(BattleModel model)
---
+++ Assets/Scripts/Battle/PlayerDefine.cs
@@ -0,0 +1,20 @@
+﻿using BattleMath;
+
+abstract class Player 
+{
+    protected vector2 location;
+    protected decimal speed;
+    public abstract void move(vector2 direction);
+    public abstract void implementTask(int key);
+} 
+class Macrophages: Player
+{
+    public override void implementTask(int key)
+    {
+    }
+
+    public override void move(vector2 direction)
+    {
+        location = vector2.plus(location, vector2.mutiply(direction, speed));
+    }
+}
