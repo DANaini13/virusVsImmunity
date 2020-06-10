9bf93d78341875b68b6e9e1cc59d787d4745bb21
Zeyong Shan <shan27@pdx.edu>
2020-06-11 00:06:06 +0800

    游戏model解析json反射调用

--- Assets/Scripts/Battle/BattleMath.cs
+++ Assets/Scripts/Battle/BattleMath.cs
@@ -2,7 +2,7 @@
 {
     struct vector2 
     { 
-        vector2(decimal x, decimal y)
+        public vector2(decimal x, decimal y)
         {
             this.x = x;
             this.y = y;
@@ -11,19 +11,30 @@
         public decimal y;
         static public vector2 plus(vector2 left, vector2 right) 
         {
-            return vector2(left.x + right.x, left.y + right.y);
+            return new vector2(left.x + right.x, left.y + right.y);
         }
         static public vector2 mines(vector2 left, vector2 right) 
         {
-            return vector2(left.x - right.x, left.y - right.y);
+            return new vector2(left.x - right.x, left.y - right.y);
         }
         static public vector2 mutiply(vector2 left, decimal mutiplier) 
         {
-            return vector2(left.x * mutiplier, left.y * mutiplier);
+            return new vector2(left.x * mutiplier, left.y * mutiplier);
         }
         static public vector2 devide(vector2 left, decimal devider) 
         {
-            return vector2(left.x/devider, left.y/devider);
+            return new vector2(left.x/devider, left.y/devider);
+        }
+        static public vector2 fromString(string str)
+        {
+            string[] parts = str.Split(new char[] { '-' });
+            decimal x = decimal.Parse(parts[0]);
+            decimal y = decimal.Parse(parts[1]);
+            return new vector2(x, y);
+        }
+        public string ToString()
+        {
+            return x + "-" + y;
         }
     }
 }
